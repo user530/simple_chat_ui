0b25ab7a2b30d06877f5db964643c73bedebef0a
user530 <m.timarsuev@gmail.com>
2023-11-10 11:20:06 +0300

    Sending and showing messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { io } from 'socket.io-client';
+
+interface ChatMessage {
+  author: string;
+  text: string;
+}
 
 function App() {
+  const socket = io('http://localhost:3001');
+
+  const [chatMessages, setChatMessages] = React.useState<ChatMessage[]>([]);
+  const [messageText, setMessageText] = React.useState<string>('');
+
+  useEffect(() => {
+    socket.emit('findAllMessages', {}, (response: ChatMessage[]) => {
+      setChatMessages(response);
+    });
+
+    const addMessage = (msg: ChatMessage) => {
+      setChatMessages(prevMessages => [...prevMessages, msg])
+    }
+
+    socket.on('message', addMessage)
+
+    return () => {
+      socket.off('message', addMessage);
+    }
+  }, [])
+
+  const sendMessage = () => socket.emit('createMessage', { text: messageText },
+    () => setMessageText(''))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chat">
+        <div className="chat-container" >
+          {chatMessages.map((message, ind) => <div className='message p-2' key={ind}>[{message.author}]: {message.text}</div>)}
+        </div>
+
+        <input type="text" value={messageText} onChange={(e) => setMessageText(e.target.value)} />
+        <input type="button" value="Send" onClick={sendMessage} />
+      </div>
     </div>
   );
 }
